bb69f3bdc7d2bf87223f2c17a6d3558962fce5c2
DickySG <june_dicky@hotmail.com>
2023-12-20 14:11:55 +1000

    Update visualize.bat

--- visualize.bat
+++ visualize.bat
@@ -1,4 +1,4 @@
-cd /d C:\Users\gozalid\Documents\LiveMap_v6
-call C:\Users\gozalid\AppData\Local\anaconda3\Scripts\activate.bat C:\Users\gozalid\AppData\Local\anaconda3
+cd /d ~\Documents\LiveMap_v6
+call ~\AppData\Local\anaconda3\Scripts\activate.bat ~\AppData\Local\anaconda3
 call activate sentosa_env
 python app.py
